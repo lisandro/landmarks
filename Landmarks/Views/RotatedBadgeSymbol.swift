e0cfb71f12c8e1f34d01b020655d8704a3c957e9
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2020-03-29 12:09:21 -0300

    Added rotated symbol to the badge

---
+++ Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Lisandro Falconi on 29/03/2020.
+//  Copyright © 2020 Lisandro Falconi. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
