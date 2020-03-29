3993260b0a5bef1ad991de75ce8e219669d8944f
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2020-03-29 11:29:02 -0300

    Drew badge background shape

---
+++ Landmarks/Views/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Lisandro Falconi on 29/03/2020.
+//  Copyright © 2020 Lisandro Falconi. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        BadgeBackground()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
