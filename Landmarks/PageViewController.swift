454b0440e426d2a185b890043c05e02c0ecb5761
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2020-03-29 18:15:38 -0300

    Added page view controller to featured landmarks

---
+++ Landmarks/PageViewController.swift
@@ -0,0 +1,61 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Lisandro Falconi on 29/03/2020.
+//  Copyright © 2020 Lisandro Falconi. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController: UIViewControllerRepresentable {
+
+    // Call before makeUIViewController
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    typealias UIViewControllerType = UIPageViewController
+
+    var controllers: [UIViewController]
+
+    // MARK: UIViewControllerRepresentable
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
+        return pageViewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIPageViewController, context: Context) {
+        uiViewController.setViewControllers([controllers[0]], direction: .forward, animated: true)
+    }
+
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+
+        init(_ pageViewController: PageViewController) {
+            self.parent = pageViewController
+        }
+
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return parent.controllers.last
+            }
+            return parent.controllers[index - 1]
+        }
+
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return parent.controllers.last
+            }
+            return parent.controllers[index - 1]
+        }
+    }
+}
